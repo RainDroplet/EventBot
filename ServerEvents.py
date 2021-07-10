4694226e78032bdf1a62ab3b48eca3d9eff5ea9f
RainDroplet <22482214+RainDroplet@users.noreply.github.com>
2021-07-10 11:06:15 -0700

    Solved the JSON read/write issue. Close #9
    Host commands now works as intended. It can pull data from a command and properly add them into the JSON file for each individual attribute of the dictionary.

--- ServerEvents.py
+++ ServerEvents.py
@@ -20,7 +20,7 @@ def create_server_events(guildID):
 
 def load_server_event(guildID):
 # This is to retrieve the JSON file that has all the events in a particular server.
-    with open(f'./server_data/{guildID}.json', 'w') as fileIn:
+    with open(f'./server_data/{guildID}.json', 'r') as fileIn:
         serverEvents = json.load(fileIn)
         return serverEvents
 
@@ -34,14 +34,15 @@ def save_server_event(guildID, serverEvents):
 async def add_server_event(guildID, discordID, title, date, time, desc):
 # This is going to be the funtion in pair with the ,host command that we wanted for the bot. Things to keep in mind are that should be able to .update the dictionary and possible check if there is already a file that has their guildID in it. Therefore there should be no need for a called once create_sever_events as a bot command.
     serverEvents = load_server_event(guildID)
+    print(serverEvents)
     print('server event loaded')
 
     newEvents = {
-        len(serverEvents): {
+        int(len(serverEvents)): {
             'Owner':discordID,
             'Title':title,
-            'Date': date,
-            'Time': time,
+            'Date':date,
+            'Time':time,
             'Desc':desc,
             'Members':[discordID]
         }
@@ -49,8 +50,8 @@ async def add_server_event(guildID, discordID, title, date, time, desc):
 
     print(newEvents)
 
-    serverEvents.append(newEvents)
-    print('server event appended')
+    serverEvents.update(newEvents)
+    print('server event updated')
 
     save_server_event(guildID,serverEvents)
     print('server event saved')
